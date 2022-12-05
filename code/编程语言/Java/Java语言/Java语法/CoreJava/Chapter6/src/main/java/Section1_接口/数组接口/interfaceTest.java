808ef5dcc2dcb4de7449685385fdeecf202176e9
coachhe <624904389@qq.com>
2022-12-06 01:45:55 +0800

    : 2022-12-06 01:45:55

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/数组接口/interfaceTest.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/数组接口/interfaceTest.java
@@ -4,6 +4,9 @@ package Section1_接口.数组接口;
  * @author CoachHe
  * @date 2022/12/6 01:42
  **/
-public class interfaceTest {
-    
+public class interfaceTest implements Comparable<Integer>{
+    @Override
+    public int compareTo(Integer o) {
+        return 0;
+    }
 }
