442094a00b59254963ee79387e0a133b97c5db2c
coachhe <624904389@qq.com>
2022-12-08 01:30:58 +0800

    : 2022-12-08 01:30:58

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C7_接口与回调/TimePrinter.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C7_接口与回调/TimePrinter.java
@@ -1,6 +1,25 @@
-package Section1_接口.C7_接口与回调;/**
+package Section1_接口.C7_接口与回调;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
+
+/**
  * @author CoachHe
  * @date 2022/12/8 01:28
-**/
-public class TimePrinter {
+ **/
+public class TimePrinter implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("At the tone, the time is "
+                + Instant.ofEpochMilli(e.getWhen()));
+        Toolkit.getDefaultToolkit().beep();
+    }
+
+    public static void main(String[] args) {
+        TimePrinter listener = new TimePrinter();
+        Timer t = new Timer(1000, listener);
+    }
 }
