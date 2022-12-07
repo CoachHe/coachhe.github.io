0c1c6ad600a07e3e05d8009d8dad2fad1536a96a
coachhe <coachhe@tencent.com>
2022-12-07 13:27:10 +0800

    : 2022-12-07 13:27:10

---
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C4_静态和私有方法/DefaultTest.java
@@ -0,0 +1,14 @@
+package Section1_接口.C4_静态和私有方法;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2022/12/7
+ * Time: 13:26
+ * Description:
+ */
+public class DefaultTest {
+    public static void main(String[] args) {
+        
+    }
+}
