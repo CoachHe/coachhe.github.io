047ec298fbbf95febb23b4286c230b5d320211b1
coachhe <624904389@qq.com>
2022-12-08 01:46:06 +0800

    : 2022-12-08 01:46:06

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C9_对象克隆/TestClone.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C9_对象克隆/TestClone.java
@@ -1,6 +1,23 @@
-package Section1_接口.C9_对象克隆;/**
+package Section1_接口.C9_对象克隆;
+
+/**
  * @author CoachHe
  * @date 2022/12/8 01:43
-**/
+ **/
 public class TestClone {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Employee original = new Employee();
+        Employee copy = original.clone();
+    }
+}
+
+class Employee implements Cloneable {
+    public Employee clone() throws CloneNotSupportedException {
+        // call Object.clone()
+        Employee cloned = (Employee) super.clone();
+
+        // clone mutable fields
+
+        return cloned;
+    }
 }
