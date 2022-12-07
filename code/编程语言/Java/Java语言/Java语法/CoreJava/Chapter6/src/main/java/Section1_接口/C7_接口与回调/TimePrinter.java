9fd91517432b5978ed2b91eac9e075798051d7a7
coachhe <624904389@qq.com>
2022-12-08 01:34:04 +0800

    : 2022-12-08 01:34:04

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C7_接口与回调/TimePrinter.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section1_接口/C7_接口与回调/TimePrinter.java
@@ -10,18 +10,22 @@ import java.time.Instant;
  * @author CoachHe
  * @date 2022/12/8 01:28
  **/
-public class TimePrinter implements ActionListener {
+public class TimePrinter {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Begin");
+        TimePrinterClass listener = new TimePrinterClass();
+        Timer t = new Timer(1000, listener);
+        t.start();
+        Thread.sleep(10000);
+    }
+}
+
+class TimePrinterClass implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("At the tone, the time is "
                 + Instant.ofEpochMilli(e.getWhen()));
         Toolkit.getDefaultToolkit().beep();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Begin");
-        TimePrinter listener = new TimePrinter();
-        Timer t = new Timer(1000, listener);
-        t.start();
     }
 }
