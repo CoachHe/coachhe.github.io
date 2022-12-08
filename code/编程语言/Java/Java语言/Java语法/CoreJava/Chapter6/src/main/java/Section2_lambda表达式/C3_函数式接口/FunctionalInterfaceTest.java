1e1bf3d2335404a433afcec27dbb51fffd1ba45a
coachhe <624904389@qq.com>
2022-12-09 00:18:30 +0800

    : 2022-12-09 00:18:30

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C3_函数式接口/FunctionalInterfaceTest.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C3_函数式接口/FunctionalInterfaceTest.java
@@ -1,6 +1,20 @@
-package Section2_lambda表达式.C3_函数式接口;/**
+package Section2_lambda表达式.C3_函数式接口;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
  * @author CoachHe
  * @date 2022/12/9 00:14
-**/
+ **/
 public class FunctionalInterfaceTest {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"abc", "efc", "fslaf", "abcded"};
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+    }
 }
