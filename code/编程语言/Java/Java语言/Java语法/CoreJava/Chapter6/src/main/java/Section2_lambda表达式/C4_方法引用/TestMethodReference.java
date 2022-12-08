48f239550ed66c93ed03e08f33848ef503207a94
coachhe <624904389@qq.com>
2022-12-09 01:32:05 +0800

    : 2022-12-09 01:32:05

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C4_方法引用/TestMethodReference.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C4_方法引用/TestMethodReference.java
@@ -1,6 +1,7 @@
 package Section2_lambda表达式.C4_方法引用;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,8 +14,10 @@ public class TestMethodReference {
     public static void main(String[] args) {
 
         Timer timer_ori = new Timer(1000, event -> System.out.println(event));
-
         Timer timer = new Timer(1000, System.out::println);
 
+        String[] strings = new String[]{"Abc", "bcd"};
+        Arrays.sort(strings, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(strings));
     }
 }
