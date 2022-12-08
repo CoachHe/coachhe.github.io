197602adf6b4c630f68a8504acaabd29224ba2d3
coachhe <coachhe@tencent.com>
2022-12-08 20:04:06 +0800

    : 2022-12-08 20:04:06

---
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C2_lambda表达式的语法/TestGrammar.java
@@ -0,0 +1,9 @@
+package Section2_lambda表达式.C2_lambda表达式的语法;/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2022/12/8
+ * Time: 20:03
+ * Description: 
+ */
+public class TestGrammer {
+}
