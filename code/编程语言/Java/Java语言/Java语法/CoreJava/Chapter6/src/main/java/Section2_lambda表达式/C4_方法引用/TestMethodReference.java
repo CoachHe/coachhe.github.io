36850ee4872fdbab03c4d2856b6750b660bad1f4
coachhe <coachhe@tencent.com>
2022-12-08 13:20:07 +0800

    : 2022-12-08 13:20:07

---
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C4_方法引用/TestMethodReference.java
@@ -0,0 +1,9 @@
+package Section2_lambda表达式.C4_方法引用;/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2022/12/8
+ * Time: 13:19
+ * Description: 
+ */
+public class TestMethodReference {
+}
