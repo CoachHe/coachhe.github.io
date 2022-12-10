6e97a1b34d6e8f01c5b044144bfe6b9d80c861fd
coachhe <coachhe>
2022-12-11 01:27:58 +0800

    : 2022-12-11 01:27:58

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C6_变量作用域/TestVariableUsageField.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter6/src/main/java/Section2_lambda表达式/C6_变量作用域/TestVariableUsageField.java
@@ -9,14 +9,20 @@ import java.awt.event.ActionListener;
  * @date 2022/12/10 18:00
  **/
 public class TestVariableUsageField {
+
+    static String text2 = "Hello2";
+
+
     public static void main(String[] args) throws InterruptedException {
         repeatMessage("Hello", 1000);
         Thread.sleep(500000);
     }
 
+    // 在lambda表达式中访问外围方法或类中的变量
     public static void repeatMessage(String text, int delay) {
         ActionListener listener = event -> {
             System.out.println(text);
+            System.out.println(text2);
             Toolkit.getDefaultToolkit().beep();
         };
         new Timer(delay, listener).start();
