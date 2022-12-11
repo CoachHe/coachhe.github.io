9ef26db3fe2ab40a8cd966287206a0125b2a37bd
coachhe <coachhe>
2022-12-12 00:50:26 +0800

    : 2022-12-12 00:50:26

--- code/大数据/实时计算/Flink/FlinkTutorial/src/main/java/com/coachhe/chapter06_WindowAndTime/时间/WatermarkTest.java
+++ code/大数据/实时计算/Flink/FlinkTutorial/src/main/java/com/coachhe/chapter06_WindowAndTime/时间/WatermarkTest.java
@@ -1,4 +1,4 @@
-package com.coachhe.chapter06_WindowAndTime;
+package com.coachhe.chapter06_WindowAndTime.时间;
 
 import com.coachhe.chapter05_DataStreamAPI.Event;
 import org.apache.flink.api.common.eventtime.*;
@@ -37,23 +37,21 @@ public class WatermarkTest {
         env.getConfig().setAutoWatermarkInterval(100);
 
         env.fromElements(
-                new Event("Mary", "./home", 1000L),
-                new Event("Bob", "./cart", 2000L),
-                new Event("Alice", "./prod?id=100", 3000L),
-                new Event("Alice", "./prod?id=200", 3500L),
-                new Event("Bob", "./prod?id=2", 2500L),
-                new Event("Alice", "./prod?id=300", 3600L),
-                new Event("Bob", "./home", 3000L),
-                new Event("Bob", "./prod?id=1", 2300L),
-                new Event("Bob", "./prod?id=3", 3300L))
+                        new Event("Mary", "./home", 1000L),
+                        new Event("Bob", "./cart", 2000L),
+                        new Event("Alice", "./prod?id=100", 3000L),
+                        new Event("Alice", "./prod?id=200", 3500L),
+                        new Event("Bob", "./prod?id=2", 2500L),
+                        new Event("Alice", "./prod?id=300", 3600L),
+                        new Event("Bob", "./home", 3000L),
+                        new Event("Bob", "./prod?id=1", 2300L),
+                        new Event("Bob", "./prod?id=3", 3300L))
                 // 有序流的watermark生成，一般做测试用
-                .assignTimestampsAndWatermarks( WatermarkStrategy.<Event>forMonotonousTimestamps()
-                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
-                            @Override
-                            public long extractTimestamp(Event event, long l) {
-                                return event.timestamp;
-                            }
-                        })) ;
+//                .assignTimestampsAndWatermarks( WatermarkStrategy.<Event>forMonotonousTimestamps() .withTimestampAssigner(new SerializableTimestampAssigner<Event>() { @Override
+//                            public long extractTimestamp(Event event, long l) {
+//                                return event.timestamp;
+//                            }
+//                        })) ;
                 // 乱序流的watermark生成
 //                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
 //                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
@@ -63,6 +61,18 @@ public class WatermarkTest {
 //                            }
 //                        })
 //                );
+                // 自定义的watermark生成
+                .assignTimestampsAndWatermarks(new WatermarkStrategy<Event>() {
+                    @Override
+                    public WatermarkGenerator<Event> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
+                        return null;
+                    }
+                }.withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                    @Override
+                    public long extractTimestamp(Event event, long l) {
+                        return event.timestamp;
+                    }
+                }));
 
 
 
