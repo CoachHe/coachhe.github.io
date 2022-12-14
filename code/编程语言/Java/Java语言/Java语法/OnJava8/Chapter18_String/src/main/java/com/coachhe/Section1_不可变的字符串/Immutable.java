c0abe3a01f1ecb7e0422915ec54740c197b9afc5
coachhe <coachhe>
2022-12-15 01:43:32 +0800

    : 2022-12-15 01:43:32

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section1_不可变的字符串/Immutable.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section1_不可变的字符串/Immutable.java
@@ -3,10 +3,15 @@ package com.coachhe.Section1_不可变的字符串;
 /**
  * @author CoachHe
  * @date 2022/12/15 01:00
- * 这里是为了展示Java字符串的不可变性，传入的其实是q对象引用的副本
  **/
 public class Immutable {
 
+    @Test
+    public void ImmutableTest() {
+        
+    }
+
+    // 这里是为了展示Java字符串的不可变性，传入的其实是q对象引用的副本
     public static String upcase(String s) {
         return s.toUpperCase();
     }
