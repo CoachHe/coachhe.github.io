2c0af32eb206584160aeb79a6a70ee3d21a6374f
coachhe <coachhe>
2022-12-15 01:53:31 +0800

    : 2022-12-15 01:53:31

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section1_不可变的字符串/Immutable.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section1_不可变的字符串/Immutable.java
@@ -1,5 +1,7 @@
 package com.coachhe.Section1_不可变的字符串;
 
+import org.junit.Test;
+
 /**
  * @author CoachHe
  * @date 2022/12/15 01:00
@@ -8,7 +10,32 @@ public class Immutable {
 
     @Test
     public void ImmutableTest() {
-        
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1 == s2);
+        s1 = "hello";
+        System.out.println(s1 == s2);
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+
+    @Test
+    public void ImmutableTest2() {
+        String s1 = "abc";
+        String s2 = "abc";
+        s2 += "def";
+        System.out.println(s2); // abcdef
+        System.out.println(s1); // abc
+    }
+
+
+    @Test
+    public void ImmutableTest3(){
+        String s1 = "abc";
+        String s2 = s1.replace("a", "m");
+        System.out.println(s1); // abc
+        System.out.println(s2); // mbc
     }
 
     // 这里是为了展示Java字符串的不可变性，传入的其实是q对象引用的副本
