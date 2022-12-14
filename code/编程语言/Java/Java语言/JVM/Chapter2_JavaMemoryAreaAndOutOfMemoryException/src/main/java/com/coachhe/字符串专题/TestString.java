1933e069ebbebc5cd2302ed2529bc0d2ea89dbae
coachhe <coachhe>
2022-12-14 19:15:29 +0800

    coachhe: 2022-12-14 19:15:28

---
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryAreaAndOutOfMemoryException/src/main/java/com/coachhe/字符串专题/TestString.java
@@ -0,0 +1,33 @@
+package com.coachhe.字符串专题;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2022/12/14
+ * Time: 19:11
+ * Description:
+ */
+public class TestString {
+
+    @Test
+    public void test3(){
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "ab";
+
+        String s4 = s1 + s2;
+        System.out.println(s3 == s4);
+    }
+
+    @Test
+    public void test4(){
+        final String s1 = "a";
+        final String s2 = "b";
+        String s3 = "ab";
+
+        String s4 = s1 + s2;
+        System.out.println(s3 == s4);
+    }
+}
