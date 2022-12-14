97a4fe21444f16d7b29e3a7fcd5cdfd351b15070
coachhe <coachhe>
2022-12-15 02:53:31 +0800

    : 2022-12-15 02:53:31

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Concatenation.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Concatenation.java
@@ -22,6 +22,30 @@ public class Concatenation {
         System.out.println(a.equals(b)); //true
     }
 
+    // 证明拼接时只要有一个变量，结果就会放入堆中
+    @Test
+    public void ConcatenationTest2(){
+        String s1 = "javaEE";
+        String s2 = "hadoop";
+
+        String s3 = "javaEEhadoop";
+        String s4 = "javaEE" + "hadoop";
+        String s5 = s1 + "hadoop";
+        String s6 = "javaEE" + s2;
+        String s7 = s1 + s2;
+
+        System.out.println(s3 == s4); // true
+        System.out.println(s3 == s5); // false
+        System.out.println(s3 == s6); // false
+        System.out.println(s3 == s7); // false
+        System.out.println(s5 == s6); // false
+        System.out.println(s5 == s7); // false
+        System.out.println(s6 == s7); // false
+
+        String s8 = s6.intern();
+        System.out.println(s3 == s8); // true
+    }
+
     public static void main(String[] args) {
         String mango = "mango";
         String s = "abc" + mango + "def" + 47;
