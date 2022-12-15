87670935ae221ad896db5893f3f6d1a0487de179
coachhe <coachhe>
2022-12-16 00:33:14 +0800

    : 2022-12-16 00:33:14

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Intern.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Intern.java
@@ -9,6 +9,14 @@ package com.coachhe.Section2_重载加号与StringBuilder;
  */
 public class Intern {
     public static void main(String[] args) {
-        "a".intern();
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2); // false
+
+        String s3 = new String("1") + new String("2");
+        s3.intern();
+        String s4 = "12";
+        System.out.println(s3 == s4); // true
     }
 }
