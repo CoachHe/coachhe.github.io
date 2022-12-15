428e59c38c77a1771be229e2f7ae9fdc61ff3a93
coachhe <coachhe>
2022-12-15 23:03:14 +0800

    : 2022-12-15 23:03:14

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/UsingStringBuilder.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/UsingStringBuilder.java
@@ -1,9 +1,37 @@
 package com.coachhe.Section2_重载加号与StringBuilder;
 
+import java.util.Random;
+import java.util.stream.Collectors;
+
 /**
  * @author CoachHe
  * @date 2022/12/15 22:49
  **/
 public class UsingStringBuilder {
-    
+
+    // 最终的结果是用append()语句对每一部分进行拼接。
+    public static String string1() {
+        Random random = new Random(47);
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < 25; i++) {
+            result.append(random.nextInt(100));
+            result.append(", ");
+        }
+        result.delete(result.length() - 2, result.length());
+        result.append("]");
+        return result.toString();
+    }
+
+    public static String string2(){
+        String result = new Random(47)
+                .ints(25, 0, 100)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", "));
+        return "[" + result + "]";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(string1());
+        System.out.println(string2());
+    }
 }
