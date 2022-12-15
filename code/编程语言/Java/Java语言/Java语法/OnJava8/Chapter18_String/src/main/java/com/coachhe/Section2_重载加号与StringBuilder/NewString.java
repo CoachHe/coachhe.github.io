705ac34010f346dc4976f91dd2ea35e66e97839e
coachhe <coachhe>
2022-12-16 00:13:14 +0800

    : 2022-12-16 00:13:14

---
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/NewString.java
@@ -0,0 +1,12 @@
+package com.coachhe.Section2_重载加号与StringBuilder;
+
+/**
+ * @author CoachHe
+ * @date 2022/12/16 00:04
+ **/
+public class NewString {
+    public static void main(String[] args) {
+//        String str = new String("ab");
+        String str = new String("a") + new String("b");
+    }
+}
