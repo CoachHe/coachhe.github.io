8234c15b30b960b5133ab5a213d04348f749cc42
coachhe <coachhe>
2022-12-16 01:23:16 +0800

    : 2022-12-16 01:23:16

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Intern.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Intern.java
@@ -58,7 +58,24 @@ public class Intern {
     // 空间效率测试
     @Test
     public void internTest5(){
-        
+        int MAX_COUNT = 1000 * 10000;
+        String[] arr = new String[MAX_COUNT];
+        Integer[] data = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < MAX_COUNT; i++) {
+            arr[i] = new String(String.valueOf(data[i % data.length])).intern();
+//            arr[i] = new String(String.valueOf(data[i % data.length]));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("花费的时间为: " + (end - start));
+
+        try {
+            Thread.sleep(1000000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
