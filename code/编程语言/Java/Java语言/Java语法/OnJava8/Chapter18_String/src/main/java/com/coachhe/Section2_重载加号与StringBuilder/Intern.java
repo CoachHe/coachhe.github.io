770c50ae4281fbffb3fc8858f400c19e2e6f0449
coachhe <coachhe>
2022-12-15 20:16:52 +0800

    coachhe: 2022-12-15 20:16:52

---
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter18_String/src/main/java/com/coachhe/Section2_重载加号与StringBuilder/Intern.java
@@ -0,0 +1,14 @@
+package com.coachhe.Section2_重载加号与StringBuilder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: coachhe
+ * Date: 2022/12/15
+ * Time: 20:09
+ * Description:
+ */
+public class Intern {
+    public static void main(String[] args) {
+        "a".intern();
+    }
+}
