f4eccd08e6143586d8aba1dbbae300fb2b99163c
coachhe <coachhe>
2022-12-21 00:36:44 +0800

    : 2022-12-21 00:36:44

--- code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter5_inheritance/src/main/java/Section1_类超类和子类/S5_多态/PolymorphicTest.java
+++ code/编程语言/Java/Java语言/Java语法/CoreJava/Chapter5_inheritance/src/main/java/Section1_类超类和子类/S5_多态/PolymorphicTest.java
@@ -10,12 +10,12 @@ import Section1_类超类和子类.S0_定义子类.Manager;
 public class PolymorphicTest {
     public static void main(String[] args) {
         Employee e;
-        e = new Manager("manager", 1, 1, 1, 1); //ok
+//        e = new Manager("manager", 1, 1, 1, 1); //ok
         e = new Employee("employee", 1, 1, 1, 1); //ok
 
         Manager[] managers = new Manager[10];
-        Employee[] staff = managers;
-        staff[0] = e;
-        System.out.println(staff[0]);
+        Employee[] staff = managers; // 此时是合理的，因为将经理转换为员工是合理的
+        staff[0] = e; // 此时好像也是合理的，因为这就是一个员工类，但实际上这应该是不合理的，因为这实际是一个经理类
+        System.out.println(staff[0]); // 直接报错ArrayStoreException
     }
 }
