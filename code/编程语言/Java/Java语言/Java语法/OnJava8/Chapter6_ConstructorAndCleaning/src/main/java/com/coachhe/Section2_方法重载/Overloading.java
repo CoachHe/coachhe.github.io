2d10e62b403005b0d70da6601c85025015c944f3
coachhe <coachhe>
2022-12-28 01:59:39 +0800

    : 2022-12-28 01:59:38

---
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter6_ConstructorAndCleaning/src/main/java/com/coachhe/Section2_方法重载/Overloading.java
@@ -0,0 +1,40 @@
+package com.coachhe.Section2_方法重载;
+
+/**
+ * @author CoachHe
+ * @date 2022/12/28 01:52
+ **/
+public class Overloading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("overloading method");
+        }
+        // 调用重载构造器
+        new Tree();
+    }
+}
+
+class Tree{
+    int height;
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+
+    Tree(int initHeight) {
+        height = initHeight;
+        System.out.println("Creating new Tree that is " + height + " feet tall");
+    }
+
+    void info(){
+        System.out.println("Tree is " + height + " feet tall");
+    }
+
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
+    }
+}
+
+
---
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter6_ConstructorAndCleaning/src/main/java/com/coachhe/Section2_方法重载/PrimitiveOverloading.java
@@ -0,0 +1,6 @@
+package com.coachhe.Section2_方法重载;/**
+ * @author CoachHe
+ * @date 2022/12/28 01:59
+**/
+public class PrimitiveOverloading {
+}
