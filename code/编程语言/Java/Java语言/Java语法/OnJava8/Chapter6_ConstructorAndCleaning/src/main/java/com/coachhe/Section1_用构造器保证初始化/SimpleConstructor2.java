bbb8a007433e2424a587c163ef39b3cb8ac5e5e3
coachhe <coachhe>
2022-12-28 00:29:38 +0800

    : 2022-12-28 00:29:38

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter6_ConstructorAndCleaning/src/main/java/com/coachhe/Section1_用构造器保证初始化/SimpleConstructor2.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter6_ConstructorAndCleaning/src/main/java/com/coachhe/Section1_用构造器保证初始化/SimpleConstructor2.java
@@ -4,16 +4,16 @@ package com.coachhe.Section1_用构造器保证初始化;
  * @author CoachHe
  * @date 2022/12/28 00:18
  **/
-public class SimpleConstructor {
+public class SimpleConstructor2 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            Rock rock = new Rock();
+            Rock2 rock2 = new Rock2(i);
         }
     }
 }
 
-class Rock {
-    Rock() { // 这个就是构造器
-        System.out.print("Rock ");
+class Rock2 {
+    Rock2(int i) { // 这个就是构造器
+        System.out.println("Rock " + i + " ");
     }
 }
