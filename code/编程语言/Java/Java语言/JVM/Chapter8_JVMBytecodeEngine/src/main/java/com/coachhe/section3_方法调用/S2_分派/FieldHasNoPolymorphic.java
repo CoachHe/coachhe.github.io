5818c2024925a81481eb2716d2247dd39425c2e3
coachhe <coachhe>
2022-12-28 11:46:47 +0800

    : 2022-12-28 11:46:47

--- code/编程语言/Java/Java语言/JVM/Chapter8_JVMBytecodeEngine/src/main/java/com/coachhe/section3_方法调用/S2_分派/FieldHasNoPolymorphic.java
+++ code/编程语言/Java/Java语言/JVM/Chapter8_JVMBytecodeEngine/src/main/java/com/coachhe/section3_方法调用/S2_分派/FieldHasNoPolymorphic.java
@@ -8,12 +8,16 @@ package com.coachhe.section3_方法调用.S2_分派;
 public class FieldHasNoPolymorphic {
     static class Father {
         public int money = 1;
+        public int home = 100;
         public Father() {
             money = 2;
             showMeTheMoney();
         }
         public void showMeTheMoney() {
             System.out.println("I am Father, i have $" + money);
+        }
+        public void showHome(){
+            System.out.println("I am Father, my home is " + home);
         }
     }
 
@@ -26,10 +30,16 @@ public class FieldHasNoPolymorphic {
         public void showMeTheMoney() {
             System.out.println("I am Son, i have $" + money);
         }
+        public void showHome(){
+            System.out.println("I am Son, my home is ");
+        }
     }
 
     public static void main(String[] args) {
         Father guy = new Son();
         System.out.println("This guy has $" + guy.money);
+        Son s = new Son();
+        int home = s.home;
+        System.out.println(home);
     }
 }
