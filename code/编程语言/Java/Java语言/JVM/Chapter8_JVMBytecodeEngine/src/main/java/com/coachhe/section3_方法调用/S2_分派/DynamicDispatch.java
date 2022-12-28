6c1a0cf106f27efe76a6055922a9c38ad7b29ecc
coachhe <coachhe>
2022-12-28 10:06:47 +0800

    : 2022-12-28 10:06:47

--- code/编程语言/Java/Java语言/JVM/Chapter8_JVMBytecodeEngine/src/main/java/com/coachhe/section3_方法调用/S2_分派/DynamicDispatch.java
+++ code/编程语言/Java/Java语言/JVM/Chapter8_JVMBytecodeEngine/src/main/java/com/coachhe/section3_方法调用/S2_分派/DynamicDispatch.java
@@ -3,7 +3,33 @@ package com.coachhe.section3_方法调用.S2_分派;
 /**
  * @author CoachHe
  * @date 2022/12/28 02:49
+ * 动态分派延时
  **/
 public class DynamicDispatch {
-    
+    static abstract class Human {
+        protected abstract void sayHello();
+    }
+
+    static class Man extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("man say hello");
+        }
+    }
+
+    static class Woman extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("woman say hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+        man.sayHello(); // man say hello
+        woman.sayHello(); // woman say hello
+        man = new Woman();
+        man.sayHello(); // woman say hello
+    }
 }
