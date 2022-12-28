fd453e7cc83a91be65f6a142b8cbd8a827382174
coachhe <coachhe>
2022-12-29 01:53:05 +0800

    : 2022-12-29 01:53:05

--- code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter19_Reflection/src/main/java/com/coachhe/Section1_为什么需要反射/Shapes.java
+++ code/编程语言/Java/Java语言/Java语法/OnJava8/Chapter19_Reflection/src/main/java/com/coachhe/Section1_为什么需要反射/Shapes.java
@@ -1,20 +1,45 @@
 package com.coachhe.Section1_为什么需要反射;
 
+import java.util.stream.Stream;
+
 /**
  * @author CoachHe
  * @date 2022/12/29 01:40
  **/
 public class Shapes {
-
+    public static void main(String[] args) {
+        Stream.of(
+                new Circle(), new Square(), new Triangle()
+        ).forEach(Shape::draw);
+    }
 }
 
 abstract class Shape{
     void draw(){
         System.out.println(this + ".draw()");
     }
+    @Override
+    public abstract String toString();
+}
 
+class Circle extends Shape {
     @Override
     public String toString() {
-        return "Shape{}";
+        return "Circle";
     }
 }
+
+class Square extends Shape {
+    @Override
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    public String toString() {
+        return "Triangle";
+    }
+}
+
