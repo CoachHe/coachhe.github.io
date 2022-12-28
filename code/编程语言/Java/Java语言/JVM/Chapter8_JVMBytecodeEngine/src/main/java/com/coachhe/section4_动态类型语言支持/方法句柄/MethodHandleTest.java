ac808fb74a3a39bedeb003185a5062f3cb1047f5
coachhe <coachhe>
2022-12-28 16:19:26 +0800

    : 2022-12-28 16:19:26

---
+++ code/编程语言/Java/Java语言/JVM/Chapter8_JVMBytecodeEngine/src/main/java/com/coachhe/section4_动态类型语言支持/方法句柄/MethodHandleTest.java
@@ -0,0 +1,30 @@
+package com.coachhe.section4_动态类型语言支持.方法句柄;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+/**
+ * @author CoachHe
+ * @date 2022/12/28 16:11
+ * 方法句柄演示
+ **/
+public class MethodHandleTest {
+    static class ClassA{
+        public void println(String s) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) throws Throwable{
+        Object obj = System.currentTimeMillis() % 2 == 0 ? System.out : new ClassA();
+        // 无论obj最终是哪个实现类，下面这句都能正确调用到println方法
+        getPrintlnMH("icyfenix");
+    }
+
+    private static MethodHandle getPrintlnMH(Object reviver) throws Throwable {
+        MethodType mt = MethodType.methodType(void.class, String.class);
+        return MethodHandles.lookup().findVirtual(reviver.getClass(), "println", mt).bindTo(reviver);
+    }
+
+}
