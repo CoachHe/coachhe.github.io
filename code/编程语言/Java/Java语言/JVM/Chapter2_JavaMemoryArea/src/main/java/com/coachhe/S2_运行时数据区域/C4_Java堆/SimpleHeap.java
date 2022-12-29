98b830f8503adc08a10bb0fa8d6ee1c054ba34bb
coachhe <coachhe>
2022-12-30 02:01:40 +0800

    : 2022-12-30 02:01:40

---
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/SimpleHeap.java
@@ -0,0 +1,25 @@
+package com.coachhe.S2_运行时数据区域.C4_Java堆;
+
+/**
+ * @author CoachHe
+ * @date 2022/12/30 01:52
+ * VM 参数：
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails
+ * 目的是为了查看堆内存的结构，运行之后查看
+ **/
+public class SimpleHeap {
+    private int id;//属性，成员变量
+
+    public SimpleHeap(int id) {
+        this.id = id;
+    }
+
+    public void show() {
+        System.out.println("My ID is " + id);
+    }
+
+    public static void main(String[] args) {
+        SimpleHeap s1 = new SimpleHeap(1);
+        SimpleHeap s2 = new SimpleHeap(2);
+    }
+}
