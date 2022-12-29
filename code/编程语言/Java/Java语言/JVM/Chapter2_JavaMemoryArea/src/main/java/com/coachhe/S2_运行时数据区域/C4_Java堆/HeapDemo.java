48f248d5303eb57bc743efad1bd9444b82dd40bc
coachhe <coachhe>
2022-12-30 01:21:40 +0800

    : 2022-12-30 01:21:40

--- code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/HeapDemo.java
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/HeapDemo.java
@@ -4,7 +4,7 @@ package com.coachhe.S2_运行时数据区域.C4_Java堆;
  * @author CoachHe
  * @date 2022/12/30 00:45
  * 用来在VisualVM中查看堆空间的分布情况，并且证明堆空间是和进程统一的（每个进程有一个堆空间）
- * VM 参数：
+ * VM 参数：-Xms10m -Xmx10m
  **/
 public class HeapDemo {
     public static void main(String[] args) {
--- code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/HeapDemo1.java
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/HeapDemo1.java
@@ -3,6 +3,8 @@ package com.coachhe.S2_运行时数据区域.C4_Java堆;
 /**
  * @author CoachHe
  * @date 2022/12/30 00:45
+ * 用来在VisualVM中查看堆空间的分布情况，并且证明堆空间是和进程统一的（每个进程有一个堆空间）
+ * VM 参数：-Xms20m -Xmx20m
  **/
 public class HeapDemo1 {
     public static void main(String[] args) {
