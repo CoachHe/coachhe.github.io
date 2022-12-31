96be073981c7e2a228e74b6a597a923de0f09b47
coachhe <coachhe>
2022-12-31 14:48:25 +0800

    : 2022-12-31 14:48:24

---
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/C4_Java堆/HeapInstance.java
@@ -0,0 +1,25 @@
+package com.coachhe.S2_运行时数据区域.C4_Java堆;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author CoachHe
+ * @date 2022/12/31 14:40
+ * 利用该程序查看堆空间各个区域的分布情况，以及Young GC情况下各个区域（S0,S1,Eden)区域的变化情况
+ **/
+public class HeapInstance {
+    byte[] buffer = new byte[new Random().nextInt(1024 * 200)];
+
+    public static void main(String[] args) {
+        ArrayList<HeapInstance> list = new ArrayList<>();
+        while (true) {
+            list.add(new HeapInstance());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S4_实战OOM/OOMPractive/DirectMemoryOOM.java
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S4_实战OOM/OOMPractive/DirectMemoryOOM.java
@@ -1,6 +1,6 @@
 package com.coachhe.S4_实战OOM.OOMPractive;
 
-import sun.misc.Unsafe;
+//import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
@@ -13,16 +13,16 @@ public class DirectMemoryOOM {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) throws Exception{
-        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
-        Class a = unsafeField.getClass();
-        Class b = Field.class;
-        Class c = Unsafe.class;
-        System.out.println(a == b);
-        System.out.println(a == c);
-        unsafeField.setAccessible(true);
-        Unsafe unsafe = (Unsafe) unsafeField.get(null);
-        while (true) {
-            unsafe.allocateMemory(_1MB);
-        }
+//        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+//        Class a = unsafeField.getClass();
+//        Class b = Field.class;
+//        Class c = Unsafe.class;
+//        System.out.println(a == b);
+//        System.out.println(a == c);
+//        unsafeField.setAccessible(true);
+//        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+//        while (true) {
+//            unsafe.allocateMemory(_1MB);
+//        }
     }
 }
