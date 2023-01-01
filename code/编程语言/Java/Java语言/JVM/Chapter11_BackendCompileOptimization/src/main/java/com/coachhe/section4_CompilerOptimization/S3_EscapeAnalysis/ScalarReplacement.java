31a533d0f923e84cd70e3767dfd8ac40e4080aa9
coachhe <coachhe>
2023-01-02 03:24:11 +0800

    : 2023-01-02 03:24:11

---
+++ code/编程语言/Java/Java语言/JVM/Chapter11_BackendCompileOptimization/src/main/java/com/coachhe/section4_CompilerOptimization/S3_EscapeAnalysis/ScalarReplacement.java
@@ -0,0 +1,30 @@
+package com.coachhe.section4_CompilerOptimization.S3_EscapeAnalysis;
+
+/**
+ * @author CoachHe
+ * @date 2023/1/2 03:14
+ * 标量替换
+ **/
+public class ScalarReplacement {
+
+    public static void main(String[] args) {
+        ScalarReplacement sr = new ScalarReplacement();
+        sr.alloc();
+    }
+
+    private void alloc() {
+        Point point = new Point(1, 2);
+        System.out.println("point.x=" + point.x + "; point.y=" + point.y);
+    }
+
+
+    class Point{
+        private int x;
+        private int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
