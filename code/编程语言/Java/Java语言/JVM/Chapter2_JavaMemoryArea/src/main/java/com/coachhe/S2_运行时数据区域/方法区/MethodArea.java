a9ea9b61ed4ccccff44deee3f314a232d96828e2
coachhe <coachhe>
2023-01-03 00:25:36 +0800

    : 2023-01-03 00:25:36

--- code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/方法区/MethodArea.java
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S2_运行时数据区域/方法区/MethodArea.java
@@ -3,10 +3,13 @@ package com.coachhe.S2_运行时数据区域.方法区;
 /**
  * @author CoachHe
  * @date 2023/1/3 00:14
+ * 验证哪怕没有实例化对象，static修饰的变量和方法也是可以访问的
  **/
 public class MethodArea {
     public static void main(String[] args) {
-        
+        Order order = null;
+        order.hello();
+        System.out.println(order.count);
     }
 }
 
