edaa301dc5442d2e60e39ab57390ce34f03fb6af
coachhe <coachhe>
2023-01-06 00:52:53 +0800

    : 2023-01-06 00:52:53

---
+++ code/编程语言/Java/Java语言/JVM/Chapter2_JavaMemoryArea/src/main/java/com/coachhe/S3_HotSpot虚拟机对象探秘/对象的访问定位/CustomerTest.java
@@ -0,0 +1,20 @@
+package com.coachhe.S3_HotSpot虚拟机对象探秘.对象的访问定位;
+
+/**
+ * @author CoachHe
+ * @date 2023/1/6 00:44
+ * 利用这个程序来对应JVM第二章笔记的图示，明确对象的访问定位在堆内存中的结构
+ **/
+public class Customer {
+    int id = 1001;
+    String name;
+    Account acct;
+
+    {
+        name = "匿名客户";
+    }
+}
+
+class Account {
+
+}
